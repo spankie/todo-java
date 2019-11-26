aa65a059f851774bcaa149dba793d1794ae1426d
Odohi David <odohi.david@gmail.com>
2019-11-26 18:18:44 +0100

    add validator class and validation annotation for the tasks status

---
+++ src/main/java/com/david/todo/validation/TaskStatusValidator.java
@@ -0,0 +1,28 @@
+package com.david.todo.validation;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class TaskStatusValidator implements ConstraintValidator<ValueOfEnum, CharSequence> {
+  private List<String> acceptedValues;
+
+  @Override
+  public void initialize(ValueOfEnum annotation) {
+      acceptedValues = Stream.of(annotation.enumClass().getEnumConstants())
+              .map(Enum::name)
+              .collect(Collectors.toList());
+  }
+
+  @Override
+  public boolean isValid(CharSequence value, ConstraintValidatorContext context) {
+      if (value == null) {
+          return true;
+      }
+
+      return acceptedValues.contains(value.toString());
+  }
+}
