2f108f89d44679ffc01ad4d419565e6614afd4cc
Odohi David <odohi.david@gmail.com>
2019-11-26 18:17:35 +0100

    add time attributes for the tasks, add some validation for the attributes as well as constructor

--- src/main/java/com/david/todo/models/Task.java
+++ src/main/java/com/david/todo/models/Task.java
@@ -1,9 +1,16 @@
 package com.david.todo.models;
 
+import java.sql.Time;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
 
 /**
  * Task
@@ -15,8 +22,26 @@ public class Task {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Integer id;
   private String status;
+
+  @NotBlank
+  @NotNull
+  @Length(max = 150)
   private String title;
   private String description;
+  @Column(name = "created_at")
+  private Time createAt;
+  @Column(name = "updated_at")
+  private Time updatedAt;
+  @Column(name = "completed_at")
+  private Time completedAt;
+
+  public Task() {}
+
+  public Task(String title, String description, String status) {
+    this.status = status;
+    this.title = title;
+    this.description = description;
+  }
 
   public Integer getId() {
     return id;
@@ -48,6 +73,30 @@ public class Task {
 
   public void setDescription(String description) {
     this.description = description;
+  }
+
+  public Time getCreateAt() {
+    return createAt;
+  }
+
+  public void setCreateAt(Time createAt) {
+    this.createAt = createAt;
+  }
+
+  public Time getUpdatedAt() {
+    return updatedAt;
+  }
+
+  public void setUpdatedAt(Time updatedAt) {
+    this.updatedAt = updatedAt;
+  }
+
+  public Time getCompletedAt() {
+    return completedAt;
+  }
+
+  public void setCompletedAt(Time completedAt) {
+    this.completedAt = completedAt;
   }
 
 }
